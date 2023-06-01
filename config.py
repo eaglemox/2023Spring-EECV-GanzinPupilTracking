9c6821fbee4bc39f064f4a88142b51e8b58dafd5
relu <eagle627@gmail.com>
2023-06-01 21:11:14 +0800

    training loop, parser done?

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
-# hyperparameter configurations
+# arguments configuration
 import argparse
 
-def args():
-    parser = argparse.ArgumentParser(description='This is our final project')
-    parser.add_argument('--epoch', )
-    parser.add_argument('--lr', )
-    parser.add_argument('--decay', )
-    parser.add_argument('--batch_size', )
+parser = argparse.ArgumentParser(description='This is our final project')
+
+# hyperparmeters
+parser.add_argument('--max_epoch', type=int, default=40, help='max epochs to train')
+parser.add_argument('--batch_size', type=int, default=16, help='batch size of training data')
+parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
+parser.add_argument('--decay', type=float, default=1e-5, help='weight decay')
+
+# file directory
+parser.add_argument('--datapath', type=str, default='./dataset', help='relative path to dataset folder')
+parser.add_argument('--model_save', type=str, default='./test', help='folder to save best model\'s .pth file')
+parser.add_argument('--log_save', type=str, default='./test', help='folder to save csvlog file')
 
 
-    return parser.parse_args()
+# arguments variable
+args = parser.parse_args()
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
+import gc
 import torch
 import numpy as np
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
 from loss import *
-from script.eval import *
-from script.utils import *
+from eval import *
+from utils import *
+from config import args
 from PIL import Image
+from time import time
 from tqdm import tqdm
 from torchsummary import summary
 from torch.nn import Conv2d
@@ -15,19 +18,20 @@ from torch.utils.data import Dataset, DataLoader, random_split
 from torch.optim.lr_scheduler import CyclicLR, CosineAnnealingLR
 from torchvision import transforms
 
+
 def get_train_path(path='./dataset'):
     data_path = path
     image_list = []
     mask_list = []
     print('Acquiring training image & mask path...')
-    for serie in tqdm(os.listdir(data_path)):
-        if serie in ['S1', 'S2', 'S3', 'S4']:
-            for sequence in os.listdir(f'{data_path}/{serie}'):
+    for subject in tqdm(os.listdir(data_path)):
+        if subject in ['S1', 'S2', 'S3', 'S4']:
+            for sequence in os.listdir(f'{data_path}/{subject}'):
                 seq_image = []
                 seq_mask = []
-                for name in os.listdir(f'{data_path}/{serie}/{sequence}'):
+                for name in os.listdir(f'{data_path}/{subject}/{sequence}'):
                     ext = os.path.splitext(name)[-1]
-                    fullpath = f'{data_path}/{serie}/{sequence}/{name}'
+                    fullpath = f'{data_path}/{subject}/{sequence}/{name}'
                     if ext == '.jpg':
                         image_list.append(fullpath)
                     elif ext == '.png':
@@ -41,11 +45,13 @@ def get_dataloader(data_dir, batch_size, split='test', valid_ratio=0.1):
     image_path, mask_path = get_train_path(data_dir)
     if split == 'train':
         transform = transforms.Compose([
+            transforms.Resize((240, 320)),
             transforms.ToTensor(),
             # TODO: add other augmentations
         ])
     else:
         transform = transforms.Compose([
+            transforms.Resize((240, 320)),
             transforms.ToTensor(),
         ])
     
@@ -55,10 +61,10 @@ def get_dataloader(data_dir, batch_size, split='test', valid_ratio=0.1):
         num_train = len(dataset) - num_valid
         train_set, valid_set = random_split(dataset, [num_train, num_valid])
         train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
-        valid_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
+        valid_loader = DataLoader(valid_set, batch_size=1, shuffle=False, num_workers=4, pin_memory=True)
         return train_loader, valid_loader
     else:
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=0, pin_memory=True, drop_last=False)
+        dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0, pin_memory=True, drop_last=False)
         return dataloader
 
 
@@ -77,7 +83,9 @@ class GanzinDataset(Dataset):
         
     def __getitem__(self, idx):
         image = Image.open(self.image_paths[idx]).convert('RGB')
-        mask = Image.open(self.mask_paths[idx]).convert('L')
+        mask_temp = Image.open(self.mask_paths[idx])
+
+        mask = mask_temp.split()[0] # origin pupil mask is [255, 0, 255]-> magenta
 
         image = self.transforms(image)
         mask = self.transforms(mask)
@@ -87,107 +95,180 @@ class GanzinDataset(Dataset):
             'masks': mask
         }
 
-def train(model, config, train_loader, valid_loader, criterion, optimizer, scheduler=None):
-    # should be in conofig
-    max_epochs = 30
-    
+def cal_batch_metric(predict_batch, label_batch, iou_list, conf_list, validframe_list):
+    predict_batch = predict_batch.cpu().detach().numpy()
+    label_batch = label_batch.cpu().detach().numpy()
+
+    for batch in range(predict_batch.shape[0]):
+        predict = predict_batch[batch]
+        label = label_batch[batch]
+
+        if np.sum(predict) > 0:
+            conf = 1.0
+            iou = mask_iou(predict, label)
+            iou_list.append(conf * iou)
+        else:  # empty ground truth label
+            conf = 0.0
+
+        if np.sum(label) > 0:
+            validframe_list.append(1.0)
+        else:  # empty ground truth label
+            validframe_list.append(0.0)
+            
+        conf_list.append(conf)
+
+def write_txtlog(log_path, current_epoch, train_score, valid_score, train_loss, valid_loss, train_iou, valid_iou, train_atnr, valid_atnr, is_better):
+    with open(log_path, 'a') as f:
+        f.write(f'[{current_epoch+1}/{args.max_epoch}] Score:{train_score}/{valid_score} | Loss:{train_loss}/{valid_loss}\
+                 | IOU:{train_iou}/{valid_iou} | ATNR:{train_atnr}/{valid_atnr}')
+        if is_better:
+            f.wrtie('--> Best Updated')
+        f.write('\n')
+
+
+def train(model, train_loader, valid_loader, criterion, optimizer, scheduler=None):
+    # list for learning curve
+    train_loss_list, valid_loss_list = [], []
+    train_score_list, valid_score_list = [], []
+
+    # initialize best loss
+    best_loss = np.inf
+
     # training
     model.train()
-    for epoch in range(max_epochs):
-        total_loss = 0.
-        iou_meter = AverageMeter()
-        iou_meter_sequence = AverageMeter()
-        label_validity = []
-        output_conf = []
+    for epoch in range(args.max_epoch):
+        print(f'Epoch: {epoch}/{args.max_epoch}')
+        train_start_time = time()
+        
+        train_loss = 0.0
+        train_iou = []
+        train_conf = []
+        train_validframe = []
+        
         for batch, data in enumerate(tqdm(train_loader, leave=True)):
             # [batch, 3, h, w], [batch, 1, h, w]
             images, masks = data['images'].to(device), data['masks'].to(device)
             # print(images.dtype, masks.dtype)
+            # print(images.shape, masks.shape)
             preds = model(images) # [batch, 1, h, w]
-            loss = criterion(preds, masks)
             # print(preds.shape)
+            loss = criterion(preds, masks)
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             # Evaluate
-            total_loss += loss.item()
-            '''from reference???'''
-            masks = masks.cpu().detach().numpy()
-            preds = preds.cpu().detach().numpy()
-
-            for batch in range(preds.shape[0]):
-                output = preds[batch]
-                label = masks[batch]
-
-                if np.sum(output.flatten()) > 0:
-                    conf = 1.0
-                    iou = mask_iou(output, label)
-                    iou_meter.update(conf * iou)
-                    iou_meter_sequence.update(conf * iou)
-                else:  # empty ground truth label
-                    conf = 0.0
-
-                if np.sum(label.flatten()) > 0:
-                    label_validity.append(1.0)
-                else:  # empty ground truth label
-                    label_validity.append(0.0)
-                    
-                output_conf.append(conf)
-        
-                pass
-    # print each epoch's result
-
-        wiou = iou_meter.avg()
-        tn_rates = true_negative_curve(np.array(output_conf), np.array(label_validity))
-        atnr = np.mean(tn_rates)
-        score = 0.7 * wiou + 0.3 * atnr
-        
-    # validation
-    model.eval()
-    for batch, data in enumerate(tqdm(valid_loader)):
-        with torch.no_grad():
-            images, masks = data['images'].to(device), data['masks'].to(device)
+            train_loss += loss.item()
             
-            preds = model(images)
-            loss = criterion(preds, masks)
+            # calculate iou, conf, N_valid
+            cal_batch_metric(preds, masks, train_iou, train_conf, train_validframe)
+            # test append in fucntion work
+            # print(len(train_iou))
 
-            total_loss += loss.item()
-    pass
+        # score metrics
+        train_wiou = np.mean(train_iou)
+        train_atnr = np.mean(true_negative_curve(np.array(train_conf), np.array(train_validframe)))
+        train_score = 0.7 * train_wiou + 0.3 * train_atnr
+        
+        train_loss_list.append(train_loss)
+        train_score_list.append(train_score)
+        train_time = time() - train_start_time
+        # validation
+        model.eval()
+        valid_loss = 0.0
+        valid_iou = []
+        valid_conf = []
+        valid_validframe = []
+
+        with torch.no_grad():
+            valid_start_time = time()
+            for data in tqdm(valid_loader):
+                images, masks = data['images'].to(device), data['masks'].to(device)
+                
+                preds = model(images)
+                loss = criterion(preds, masks)
+
+                valid_loss += loss.item()
+
+                # calculate iou, conf, N_valid
+                cal_batch_metric(preds, masks, train_iou, train_conf, train_validframe)
+
+        valid_wiou = np.mean(valid_iou)
+        valid_atnr = np.mean(true_negative_curve(np.array(valid_conf), np.array(valid_validframe)))
+        valid_score = 0.7 * valid_wiou + 0.3 * valid_atnr
+        
+        valid_loss_list.append(valid_loss)
+        valid_score_list.append(valid_score)
+        valid_time = time() - valid_start_time
+
+        # print each epoch's result: loss, score
+        print(f'[{epoch + 1}/{args.max_epoch}] \
+              {train_time:.2f}/{valid_time:.2f} sec(s) Score: {train_score:.3f}/{valid_score:.3f} | Loss: {train_loss:.3f}/{valid_loss:.3f}')
+        
+        # update scheduler
+        scheduler.step()
+
+        # check & update best loss
+        is_better = valid_loss <= best_loss
+        best_loss = valid_loss
+        # save model
+        if is_better:
+            os.makedirs(args.model_save, exist_ok=True)
+            torch.save(model.state_dict(), os.path.join(args.model_save, f'model_best_{epoch}.pth'))
+        # write textlog
+        os.makedirs(args.log_save, exist_ok=True)
+        write_txtlog(os.path.join(args.log_save, 'log.txt'), epoch, train_score, valid_score, train_loss, valid_loss\
+                     , train_iou, valid_iou, train_atnr, valid_atnr, is_better)
+
 
 if __name__ == '__main__':
+    '''Choose Device'''
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Device:', device)
     
-    train_loader, valid_loader = get_dataloader('./dataset', batch_size=16, split='train', valid_ratio=0.1)
-    # for data in trainset:
+    '''Get DataLoader'''
+    train_loader, valid_loader = get_dataloader(args.datapath, batch_size=args.batch_size, split='train', valid_ratio=0.1)
+    # for batch, data in enumerate(train_loader):
+    #     print(data['masks'].shape)
+        # fig = plt.figure()
+        # plt.imshow(data['masks'][0].permute(1,2,0))
+        # plt.show()
         # print(type(data['images']), type(data['masks']))
         # print(data['images'].shape, data['masks'].shape)
         # print(len(data['masks'].split()))
-    
+
+    '''Pretrained Model Selection'''
+    # https://github.com/mateuszbuda/brain-segmentation-pytorch
     model = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet', in_channels=3, out_channels=1, init_features=32, pretrained=True)
-    # model.conv = Conv2d(32, 4, kernel_size=(1, 1), stride=(1, 1))
+    # https://github.com/milesial/Pytorch-UNet
+    # model = torch.hub.load('milesial/Pytorch-UNet', 'unet_carvana', pretrained=True, scale=0.5)
+    # model.outc.conv = Conv2d(64, 1, kernel_size=(1, 1), stride=(1, 1)) # unet_carvana outpur is 4 channel
+    
+    '''Model Setting & Summary'''
     model = nn.DataParallel(model, device_ids=[0, 1])
     model.to(device)
-    # summary(model, input_size=(3, 480, 640))
+    summary(model, input_size=(3, 240, 320))
     # print(model)
-
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+    
+    '''Selecting optimizer ...'''
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.decay)
     criterion = DiceLoss()
     # nn.CrossEntropyLoss()
-    scheduler = CosineAnnealingLR(optimizer, T_max=30)
+    scheduler = CosineAnnealingLR(optimizer, T_max=args.max_epoch)
     
+    '''Start Training'''
     train(model=model,
-          config='to be added',
           train_loader=train_loader,
           valid_loader=valid_loader,
           criterion=criterion,
           optimizer=optimizer,
-        #   scheduler=scheduler
+          scheduler=scheduler
           )
 
 
     '''
+    # This is useless #
     model.eval()
     input_image = Image.open('./dataset/S1/01/0.jpg').convert('RGB')
 
@@ -196,13 +277,17 @@ if __name__ == '__main__':
     ])
     input_tensor = preprocess(input_image)
     print(input_tensor.shape)
-    input_batch = input_tensor.unsqueeze(0)
+    input_batch = input_tensor.unsqueeze(0).to(device)
     
     with torch.no_grad():
         output = model(input_batch)
         print(output.shape)
-    out = (output.numpy() * 255).astype(np.uint8)[0]
+    out_np = output.cpu().numpy()
+    print(np.unique(out_np))
+    out = ((out_np -np.min(out_np)) / (np.max(out_np) - np.min(out_np) )* 255).astype(np.uint8)[0, 0]
     print(out.shape)
-    im = Image.fromarray(out.transpose((1, 2, 0)))
+    # cv2.imshow("test", out)
+    # cv2.waitKey(0)
+    im = Image.fromarray(out.transpose(0, 1))
     im.save('./ans.png')
     '''
