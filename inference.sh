dde71e2ffcfef376bcd71213832e90aeaa44108b
relu <eagle627@gmail.com>
2023-06-13 16:21:57 +0800

    final versionn-1

--- inference.sh
+++ inference.sh
@@ -1,5 +1,5 @@
 python3 inference.py --batch_size 64 \
-                     --model_path './codalab_best.pth' \
+                     --model_path '.codalab_best.pth' \
                      --data_path './dataset/' --output_path './mask/' --testset True
 
 # for arguments description
